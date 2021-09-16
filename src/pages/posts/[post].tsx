3443be6f3a347311b81d121599ef271c08038186
Rod Midas <rodmidas@tuta.io>
2021-09-16 02:35:30 -0700

    remove things

--- src/pages/posts/[post].tsx
+++ src/pages/posts/[post].tsx
@@ -9,9 +9,6 @@ import yaml from "js-yaml";
 import { parseISO } from 'date-fns';
 import PostLayout from "../../components/PostLayout";
 
-import InstagramEmbed from "react-instagram-embed";
-import YouTube from "react-youtube";
-import { TwitterTweetEmbed } from "react-twitter-embed";
 
 export type Props = {
   title: string;
@@ -23,7 +20,7 @@ export type Props = {
   source: MdxRemote.Source;
 };
 
-const components = { InstagramEmbed, YouTube, TwitterTweetEmbed };
+const components = { };
 const slugToPostContent = (postContents => {
   let hash = {}
   postContents.forEach(it => hash[it.slug] = it)
